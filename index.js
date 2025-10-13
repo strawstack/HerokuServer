95874c6298fba7f585d17153c75622124a64023a
RichardHayes <rhshadowgreen@gmail.com>
2025-10-12 21:52:13 -0700

    Basic node server that serves index html on root

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-const express = require('express')
-const path = require('path')
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import express from 'express';
+import path from 'path';
 
-const port = process.env.PORT || 5006
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const app = express()
+const port = process.env.PORT || 5006;
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'src')));
 
-app.get('/', (req, res) => {
-  console.log(`Rendering 'pages/index' for route '/'`)
-  res.render('pages/index')
-})
+//
+// Routes
+//
+app.get('/', (_, res) => {
+  res.sendFile('src/index.html');
+});
 
+//
+// Start Server
+//
 const server = app.listen(port, () => {
-  console.log(`Listening on ${port}`)
-})
+  console.log(`http://localhost:${port}`)
+});
 
-// The number of seconds an idle Keep-Alive connection is kept open. This should be greater than the Heroku Router's
-// Keep-Alive idle timeout of 90 seconds:
-// - to ensure that the closing of idle connections is always initiated by the router and not the Node.js server
-// - to prevent a race condition if the router sends a request to the app just as Node.js is closing the connection
+//
+// Config
+//
 // https://devcenter.heroku.com/articles/http-routing#keepalives
 // https://nodejs.org/api/http.html#serverkeepalivetimeout
-server.keepAliveTimeout = 95 * 1000
-
+server.keepAliveTimeout = 95 * 1000;
 process.on('SIGTERM', async () => {
-  console.log('SIGTERM signal received: gracefully shutting down')
+  console.log('SIGTERM signal received: gracefully shutting down');
   if (server) {
     server.close(() => {
-      console.log('HTTP server closed')
+      console.log('HTTP server closed');
     })
   }
 })
