b4f718929146e9123ccb51f3951bd5a5a3ad4054
Richard Hayes <rhshadowgreen@gmail.com>
2025-12-26 21:53:48 -0800

    Add /p/projects and /p/projects/get to allow myself to sign in with Github, generate a list of projects stored in a file on the server, and get that project list as json

--- src/p/projects/routes.js
+++ src/p/projects/routes.js
@@ -1,26 +1,59 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { readFileSync, writeFileSync } from 'fs';
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export function routes({ get }) {
     get({
-            route: "/projects",
+            route: "/",
             query: [],
             desc: "Visit this page to update the list of projects",
             rtn: "",
-            callback: (req, res) => {
-                res.sendFile(`site/index.html`, {root: __dirname});
+            callback: async (req, res) => {
+                const { code } = req.query;
+                if (code) {
+                    const response = await fetch("https://github.com/login/oauth/access_token", {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            client_id: process.env.GITHUB_CLIENT_ID,
+                            client_secret: process.env.GITHUB_CLIENT_SECRET,
+                            code: code,
+                        }),
+                    });
+                    const data = await response.json();
+                    const { access_token } = data;
+
+                    var headers = new Headers();
+                    headers.append("Accept", "application/json");
+                    headers.append("X-GitHub-Api-Version", "2022-11-28");
+                    headers.append("Authorization", `Bearer ${access_token}`);
+                    const requestOptions = {
+                        method: 'GET',
+                        headers: headers,
+                    };
+                    const get_response = await fetch("https://api.github.com/users/strawstack/repos", requestOptions);
+                    const get_data = await get_response.json();
+                    writeFileSync(`${__dirname}/data/projects.json`, JSON.stringify(get_data));
+                    res.send("/p/projects/get");
+                } else {
+                    res.sendFile(`site/index.html`, {root: __dirname});
+                }
             }
         }
     );
     get({
-            route: "/projects/get",
-            query: [],
-            desc: "Visit this page to update the list of projects",
-            rtn: "Returns a list of my github public projects",
-            callback: (req, res) => {
-                res.send("todo");
-            }
+        route: "/get",
+        query: [],
+        desc: "Visit this page to update the list of projects",
+        rtn: "JSON object containing a list of my Github projects",
+        callback: async (req, res) => {
+            const content = readFileSync(`${__dirname}/data/projects.json`);
+            res.send(JSON.parse(content));
         }
-    );
+    });
 }
